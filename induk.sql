0f69509277e68506a90f7c89cfae6565b046a944
Faizul Amali <faizuloke@gmail.com>
2018-09-10 11:39:50 +0700

    Database

---
+++ induk.sql
@@ -0,0 +1,49 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: 10 Sep 2018 pada 06.39
+-- Versi Server: 10.1.19-MariaDB
+-- PHP Version: 5.6.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pedatren`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `induk`
+--
+
+CREATE TABLE `induk` (
+  `uuid` char(36) NOT NULL,
+  `nama_lengkap` varchar(35) NOT NULL,
+  `kelas` varchar(10) NOT NULL,
+  `nomor_induk` varchar(18) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `induk`
+--
+ALTER TABLE `induk`
+  ADD PRIMARY KEY (`uuid`),
+  ADD UNIQUE KEY `nomor_induk` (`nomor_induk`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
